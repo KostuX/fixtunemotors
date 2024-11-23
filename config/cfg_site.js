3f97ff1fe538b004fe1d9b9849460f78ca0b2d20
KostuX <77516813+KostuX@users.noreply.github.com>
2024-11-23 19:27:34 +0000

    phone number

--- config/cfg_site.js
+++ config/cfg_site.js
@@ -1,12 +1,12 @@
 export const cfg_site = {
   title: "Fix Tune Motors",
   year: "2024",
-  phone: ["+353 087 278 8606", "+353 087 466 3350"],
+  phone: ["087 278 8606", "087 466 3350"],
   email: [""],
   address: "Latt, Co. Cavan",
   postCode: "H12 X938",
   googleMap:
-    "https://www.google.com/maps/dir/H12+X938,+Latt,+Co.+Cavan/@54.011154,-7.4430384,12z",
+    "https://www.google.com/maps?saddr=My+Location&daddr=Fix+Tune+Motors",
   slogan: [
     "From Tune-ups to Repairs, We Keep You Moving.",
     "Providing Expert Maintenance, Reliable Fixes, and Unmatched Customer Care for Every Mile You Drive!",
