48ca36f6d6c27e0a2aa3ecdf9b60497431b32c72
KostuX <77516813+KostuX@users.noreply.github.com>
2024-12-01 20:21:49 +0000

    video max width

--- components/about/video.js
+++ components/about/video.js
@@ -1,16 +1,11 @@
 export default function Video() {
-    return (
-        <div className="relative w-full bg-black overflow-hidden h-fit overflow-hidden">
-            <video
-                preload="auto"
-                autoPlay
-                muted
-                loop
-                className="w-screen"
-            >
-                <source src="/video/fixtunemotors.mp4" type="video/mp4" />
-            </video>
-        </div>
-
-    )
+  return (
+    <div className="flex justify-center">
+      <div className="relative w-full max-w-6xl bg-black overflow-hidden h-fit overflow-hidden">
+        <video preload="auto" autoPlay muted loop className="w-screen">
+          <source src="/video/fixtunemotors.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
 }
