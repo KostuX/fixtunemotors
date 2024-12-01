0feb7ae8d41aa0f2df04ae0570276bd373ccc904
KostuX <77516813+KostuX@users.noreply.github.com>
2024-12-01 18:14:38 +0000

    foto

--- config/cfg_services.js
+++ config/cfg_services.js
@@ -5,92 +5,108 @@ export const cfg_services = {
       title: "Remapping ",
       description:
         "Unlock your car’s full potential with our expert remapping services! Experience enhanced power, improved fuel efficiency, and smoother performance. Tailored to your vehicle, our remaps maximize engine potential while maintaining safety. Drive smarter, stronger, and more efficiently—upgrade today and feel the difference! Performance and savings, all in one tune.",
-      description_short:"Unlock your car’s potential with expert remapping: enhanced power, fuel efficiency, smoother performance, and smarter driving. Upgrade now!",
-        price: "150",
-      image: "/services/remap.jpg",
+      description_short:
+        "Unlock your car’s potential with expert remapping: enhanced power, fuel efficiency, smoother performance, and smarter driving. Upgrade now!",
+      price: "150",
+      image: "/services/remap.png",
       link: "",
-      boost:{type:"Boost your car by: ", size:50}
+      boost: { type: "Boost your car by: ", size: 50 },
     },
     // title 2
     {
       title: "DPF, EGR",
       description:
         "Improved Engine Performance Reduced Backpressure: DPF reprogramming often removes or reduces restrictions, which decreases exhaust backpressure, improving engine efficiency and performance. Improved Airflow: Disabling or reprogramming the EGR reduces the amount of exhaust gas recirculated into the intake, enhancing airflow and increasing power. Enhanced Throttle Response: With less restriction from emissions systems, throttle response can become sharper, providing a more dynamic driving experience.",
-      description_short:"Improved engine performance, reduced backpressure, better airflow, and enhanced throttle response. DPF reprogramming and EGR adjustments boost efficiency and power.",
-        price: "250",
+      description_short:
+        "Improved engine performance, reduced backpressure, better airflow, and enhanced throttle response. DPF reprogramming and EGR adjustments boost efficiency and power.",
+      price: "250",
       image: "/services/pdf.png",
       link: "",
-      boost:{type:"Boost your car by: ", size:50}
+      boost: { type: "Boost your car by: ", size: 50 },
     },
     // title 2
     {
       title: "Tyre",
-      description: "Flat tire? Don’t worry! Our expert tire repair services ensure quick, reliable fixes to get you back on the road. We provide affordable solutions, quality workmanship, and safety-focused repairs for all types of tires. Trust us for durable results and smooth rides. Visit us today for hassle-free service!",
-      description_short:"Quick, reliable tire repairs to keep you safe on the road. Fast service, expert care, and affordable prices!",
+      description:
+        "Flat tire? Don’t worry! Our expert tire repair services ensure quick, reliable fixes to get you back on the road. We provide affordable solutions, quality workmanship, and safety-focused repairs for all types of tires. Trust us for durable results and smooth rides. Visit us today for hassle-free service!",
+      description_short:
+        "Quick, reliable tire repairs to keep you safe on the road. Fast service, expert care, and affordable prices!",
       price: "10",
       image: "/services/tire_repair.png",
       link: "",
-      boost:{type:"Increase car safety by* :", size:80}
+      boost: { type: "Increase car safety by* :", size: 80 },
     },
     // title 3
     {
       title: "Engine",
-      description: "Revitalize your car's heart with our expert engine services! From diagnostics to complete rebuilds, we ensure peak performance, fuel efficiency, and reliability. Our skilled technicians use advanced tools to deliver precision care for all engine types. Drive with confidence—trust us to keep your engine running like new!",
-      description_short:"Expert engine repairs to restore performance and reliability. Fast, affordable service ensuring your vehicle runs smoothly and efficiently.",
+      description:
+        "Revitalize your car's heart with our expert engine services! From diagnostics to complete rebuilds, we ensure peak performance, fuel efficiency, and reliability. Our skilled technicians use advanced tools to deliver precision care for all engine types. Drive with confidence—trust us to keep your engine running like new!",
+      description_short:
+        "Expert engine repairs to restore performance and reliability. Fast, affordable service ensuring your vehicle runs smoothly and efficiently.",
       price: "250",
       image: "/services/engine.png",
       link: "",
-      boost:{type:"Increase Car life span by* : ", size:75}
+      boost: { type: "Increase Car life span by* : ", size: 75 },
     },
     // title 4
     {
       title: "Service",
-      description: "Keep your car in top shape with our comprehensive car service! From oil changes to full inspections, we ensure peak performance, safety, and reliability. Affordable, fast, and trusted by drivers—let our expert technicians care for your car. Drive confidently with our top-notch maintenance services. Book your service today!",
-      description_short:"Reliable car service for all your maintenance needs. Expert technicians, fast service, and affordable prices to keep you on the road.",
+      description:
+        "Keep your car in top shape with our comprehensive car service! From oil changes to full inspections, we ensure peak performance, safety, and reliability. Affordable, fast, and trusted by drivers—let our expert technicians care for your car. Drive confidently with our top-notch maintenance services. Book your service today!",
+      description_short:
+        "Reliable car service for all your maintenance needs. Expert technicians, fast service, and affordable prices to keep you on the road.",
       price: "50",
       image: "/services/service.png",
       link: "",
-      boost:{type:"Boost Car life span by* : ", size:90}
+      boost: { type: "Boost Car life span by* : ", size: 90 },
     },
     // title 5
     {
       title: "Diagnostic",
-      description: "Uncover hidden issues with our advanced car diagnostic service! Using state-of-the-art tools, we identify problems quickly and accurately. From engine lights to performance checks, our experts ensure your vehicle runs smoothly and safely. Affordable, reliable, and fast diagnostics for peace of mind. Drive smarter—book your diagnostic appointment today!",
-      description_short:"Accurate car diagnostics to identify and fix issues quickly. Expert analysis, fast service, and reliable solutions for a smooth drive.",
+      description:
+        "Uncover hidden issues with our advanced car diagnostic service! Using state-of-the-art tools, we identify problems quickly and accurately. From engine lights to performance checks, our experts ensure your vehicle runs smoothly and safely. Affordable, reliable, and fast diagnostics for peace of mind. Drive smarter—book your diagnostic appointment today!",
+      description_short:
+        "Accurate car diagnostics to identify and fix issues quickly. Expert analysis, fast service, and reliable solutions for a smooth drive.",
       price: "75",
       image: "/services/diagnostic.png",
       link: "",
-      boost:{type:"Detect issues faster by* : ", size:50}
+      boost: { type: "Detect issues faster by* : ", size: 50 },
     },
     // title 2
-    
+
     {
       title: "Gearbox & Clutch",
-      description: "Smooth shifting, worry-free driving! Our expert gearbox and clutch services ensure optimal performance, durability, and reliability. Whether it's repairs, replacements, or diagnostics, we handle it all with precision and care. Affordable solutions and quick turnaround for a hassle-free experience. Keep your car running strong—trust us for gearbox and clutch excellence!",
-      description_short:"Expert gearbox and clutch repairs for smooth shifting and optimal performance. Fast, reliable service to keep your ride in gear!",
+      description:
+        "Smooth shifting, worry-free driving! Our expert gearbox and clutch services ensure optimal performance, durability, and reliability. Whether it's repairs, replacements, or diagnostics, we handle it all with precision and care. Affordable solutions and quick turnaround for a hassle-free experience. Keep your car running strong—trust us for gearbox and clutch excellence!",
+      description_short:
+        "Expert gearbox and clutch repairs for smooth shifting and optimal performance. Fast, reliable service to keep your ride in gear!",
       price: "250",
       image: "/services/gearbox.png",
       link: "",
-      boost:{type:"Boost Car Performance By* : ", size:50}
+      boost: { type: "Boost Car Performance By* : ", size: 50 },
     },
     // title 2
     {
       title: "Timing Belts / Chains",
-      description: "Protect your engine with a professional timing belt replacement! Our expert service ensures precision, reliability, and long-lasting performance. Avoid costly damage with timely maintenance. Affordable, fast, and trusted by drivers—keep your engine running smoothly and efficiently. Don’t wait—schedule your timing belt service today for peace of mind on the road!",
-      description_short:"Ensure smooth engine performance with a quick, reliable timing belt change. Affordable, efficient, preventative maintenance keeps you driving safely!",
+      description:
+        "Protect your engine with a professional timing belt replacement! Our expert service ensures precision, reliability, and long-lasting performance. Avoid costly damage with timely maintenance. Affordable, fast, and trusted by drivers—keep your engine running smoothly and efficiently. Don’t wait—schedule your timing belt service today for peace of mind on the road!",
+      description_short:
+        "Ensure smooth engine performance with a quick, reliable timing belt change. Affordable, efficient, preventative maintenance keeps you driving safely!",
       price: "450",
       image: "/services/timingbelt.png",
       link: "",
-      boost:{type:"Boost Car life span by* : ", size:80}
+      boost: { type: "Boost Car life span by* : ", size: 80 },
     },
     {
       title: "Breaks",
-      description: "Protect your engine with a professional timing belt replacement! Our expert service ensures precision, reliability, and long-lasting performance. Avoid costly damage with timely maintenance. Affordable, fast, and trusted by drivers—keep your engine running smoothly and efficiently. Don’t wait—schedule your timing belt service today for peace of mind on the road!",
-      description_short:"Drive with confidence! Expert car brake repairs ensure safety, reliability, precision, and durability. Affordable, quick service for smooth stops today!",
+      description:
+        "Protect your engine with a professional timing belt replacement! Our expert service ensures precision, reliability, and long-lasting performance. Avoid costly damage with timely maintenance. Affordable, fast, and trusted by drivers—keep your engine running smoothly and efficiently. Don’t wait—schedule your timing belt service today for peace of mind on the road!",
+      description_short:
+        "Drive with confidence! Expert car brake repairs ensure safety, reliability, precision, and durability. Affordable, quick service for smooth stops today!",
       price: "20",
       image: "/services/breakes.png",
       link: "",
-      boost:{type:"Increase car safety by* : ", size:50}
+      boost: { type: "Increase car safety by* : ", size: 50 },
     },
   ],
 };
