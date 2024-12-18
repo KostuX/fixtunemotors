a6914ce50b7484904a5a095be7ed8aa12f80e74a
KostuX <77516813+KostuX@users.noreply.github.com>
2024-12-18 00:02:18 +0000

    Update ScrollContext.js

--- components/ScrollContext.js
+++ components/ScrollContext.js
@@ -9,16 +9,6 @@ export default function ScrollContext({ children }) {
   const [lenisRef, setLenis] = useState(null);
   const [rafState, setRaf] = useState(null);
 
-  let lenisOptions = {
-    // Valeur entre 0 et 1
-    // Valeur par défaut : 0,1
-    // Plus la valeur est faible, plus le scroll sera fluide
-    lerp: 1.05,
-    // Valeur par défaut : 1
-    // Plus la valeur est haute, plus le défilement sera rapide
-    wheelMultiplier: -10,
-  };
-
   useEffect(() => {
     const scroller = new Lenis({ lerp: 0.1 });
     let rf;
