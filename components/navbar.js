939c3511e8e3bdbb706e86b13ec44c2da2ed8d26
KostuX <konstantinas.avlasevicius@combilift.com>
2025-04-25 18:05:46 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -20,11 +20,12 @@ import NextLink from "next/link";
 
 export const Navbar = ({ siteData, fonts }) => {
   const { resolvedTheme } = useTheme(); 
+
   const [isHydrated, setIsHydrated] = useState(false);
 
   useEffect(() => {
     setIsHydrated(true);
-  }, []);
+  }, [resolvedTheme]);
 
   return (
     <>
@@ -51,7 +52,118 @@ export const Navbar = ({ siteData, fonts }) => {
             </NextLink>
           </NavbarBrand>
         </NavbarContent>
-        {/* ...rest of the code */}
+
+        {/** Links | center | large */}
+        <NavbarContent className="hidden sm:flex" justify="center">
+          {cfg.navItems.map((e) => (
+            <NavbarItem key={e.label}>
+              <NextLink
+                className="flex justify-start items-center m-1 "
+                href={e.href}
+                key={e.label}
+              >
+                {e.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+
+        {/** Links | end | large */}
+        <NavbarContent
+          className="hidden sm:flex basis-1/5 sm:basis-full"
+          justify="end"
+        >
+          <NavbarItem className="hidden sm:flex gap-2">
+            <ThemeSwitch />
+          </NavbarItem>
+        </NavbarContent>
+
+        {/** Menu | end | large */}
+        <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+          <ThemeSwitch />
+          <NavbarMenuToggle />
+
+          <NavbarMenu>
+            <div className="h-fit pb-24">
+              <div className={`${fonts.marker.className} mt-10 text-center `}>
+                {cfg.navMenuItems.map((item, index) => (
+                  <NavbarMenuItem key={`${item}-${index}`}>
+                    <Link
+                      color={"foreground"}
+                      href={item.href}
+                      size="lg"
+                      className=" group transition-all duration-300 ease-in-out mt-10 text-4xl"
+                    >
+                      <p className=" bg-left-bottom bg-gradient-to-r from-green-500 to-orange-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                        {item.label}
+                      </p>
+                    </Link>
+                  </NavbarMenuItem>
+                ))}
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-3 text-center mt-10">
+                <Divider />
+                <p className={`${fonts.marker.className} mx-2 mt-12`}>Hours</p>
+                <ul>
+                  {siteData.opening_hours.weekday_text.map((hours, i) => (
+                    <li key={i}>{hours}</li>
+                  ))}
+                </ul>
+
+                <div>
+                  <p className={`${fonts.marker.className} mx-2 mt-10`}>
+                    Phone
+                  </p>
+                  <ul>
+                    {siteData.phone.map((phone, i) => (
+                      <NextLink key={i} href={`tel:${phone}`}>
+                        <div> {phone}</div>
+                      </NextLink>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <NextLink href={siteData.googleMap}>
+                    <p className={`${fonts.marker.className} mx-2 mt-10`}>
+                      Address
+                    </p>
+                    {siteData.address.map((address, i) => (
+                      <p key={i}>{address.long_name}</p>
+                    ))}
+                  </NextLink>
+                </div>
+                <div>
+                  <NextLink href={`mailto:${siteData.email[0]}`}>
+                    <p className={`${fonts.marker.className} mx-2 mt-10`}>
+                      Email
+                    </p>
+                    {siteData.email.map((email, i) => (
+                      <p key={i}>{email}</p>
+                    ))}
+                  </NextLink>
+                </div>
+              </div>
+              <div>
+                <NavbarBrand className=" mt-10 flex justify-center">
+                  <NextLink className="  " href="/">
+                    {isHydrated && (
+                      <Image
+                        src={`${
+                          resolvedTheme === "dark"
+                            ? "/logo/logo_white.png"
+                            : "/logo/logo_black.png"
+                        }`}
+                        width={120}
+                        height={120}
+                        alt="Company Logo"
+                      />
+                    )}
+                  </NextLink>
+                </NavbarBrand>
+              </div>
+            </div>
+          </NavbarMenu>
+        </NavbarContent>
       </NextUINavbar>
     </>
   );
