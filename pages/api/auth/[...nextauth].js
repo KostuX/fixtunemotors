294257bf87ff2d1fbfeaddc8db18f1d5b9a8b7fc
KostuX <konstantinas.avlasevicius@combilift.com>
2025-04-26 22:35:27 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -22,7 +22,7 @@ export const authOptions = {
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "process.env.NEXTAUTH_SECRET",
   pages: {
     signIn: "/auth/signin", // Custom sign-in page
   },
