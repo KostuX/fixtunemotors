0bac6d7e8ceb50b5d7d75023f0e197f890cbeae3
KostuX <konstantinas.avlasevicius@combilift.com>
2025-04-27 10:27:13 +0100

    nextUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 import { cfg_site as cfg } from "./config/cfg_site";
-
+import { nextui } from "@nextui-org/react";
 export const content = [
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   "./components/**/*.{js,ts,jsx,tsx,mdx}",
   "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}", // Include HeroUI components
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 ];
 
 export const theme = {
-  extend: {
-    colors: {
-      theme_secondary_dark: cfg.theme_secondary_dark,
-      theme_secondary_light: cfg.theme_secondary_light,
-    },
-  },
+  extend: {},
 };
-
-export const darkMode = "class"; // Enable dark mode with the "class" strategy
-
-export const plugins = []; // Remove the invalid HeroUI Tailwind plugin
+export const darkMode = "class";
+export const plugins = [
+  nextui({
+    themes: {
+      dark: {
+        colors: {
+          theme_secondary: cfg.theme_secondary_dark,
+        },
+      },
+      light: {
+        colors: {
+          theme_secondary: cfg.theme_secondary_light,
+        },
+      },
+    },
+  }),
+];
