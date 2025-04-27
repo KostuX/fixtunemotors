9bd2d6922e92c21a4e03b8133a326f4ff538472a
KostuX <konstantinas.avlasevicius@combilift.com>
2025-04-27 13:11:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@tanstack/react-virtual"],
+  experimental: {
+    esmExternals: "loose",
+  },
 };
 
 export default nextConfig;
